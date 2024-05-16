61bf6f0104e9a7cea9f5ee8bbb49fb2132c4211e
Alex <alex@polyform.co>
2024-05-16 10:23:33 +0200

    Completed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -11,26 +11,40 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var changeTrafficLightButton: UIButton!
     
-    
     @IBOutlet weak var redLightView: UIView!
     @IBOutlet weak var yellowLightView: UIView!
     @IBOutlet weak var greenLightView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         changeTrafficLightButton.layer.cornerRadius = 8
         
         createCircleView(redLightView)
         createCircleView(yellowLightView)
         createCircleView(greenLightView)
-        
-        // Do any additional setup after loading the view.
     }
     
     func createCircleView(_ view: UIView) {
         view.layer.cornerRadius = view.frame.size.width / 2
+        view.clipsToBounds = true
     }
 
-
+    @IBAction func changeTrafficLightButtonDidTapped(_ sender: UIButton) {
+        sender.setTitle("Next", for: .normal)
+        
+        if redLightView.alpha == 1.0 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1.0
+        } else if yellowLightView.alpha == 1.0 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1.0
+        } else if greenLightView.alpha == 1.0 {
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1.0
+        } else { 
+            redLightView.alpha = 1.0
+        }
+    }
+    
 }
-
