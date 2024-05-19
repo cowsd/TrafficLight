83d22b67fa729a37f9e63a88457c988425562fc5
Alex <alex@polyform.co>
2024-05-19 19:36:18 +0200

    erros fixed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,44 +7,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     @IBOutlet weak var changeTrafficLightButton: UIButton!
     
     @IBOutlet weak var redLightView: UIView!
     @IBOutlet weak var yellowLightView: UIView!
     @IBOutlet weak var greenLightView: UIView!
     
+    private var isFirstPress = true
+
+    private var currentState = CurrentLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         changeTrafficLightButton.layer.cornerRadius = 8
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
         createCircleView(redLightView)
         createCircleView(yellowLightView)
         createCircleView(greenLightView)
     }
     
-    func createCircleView(_ view: UIView) {
-        view.layer.cornerRadius = view.frame.size.width / 2
-        view.clipsToBounds = true
-    }
-
     @IBAction func changeTrafficLightButtonDidTapped(_ sender: UIButton) {
-        sender.setTitle("Next", for: .normal)
-        
-        if redLightView.alpha == 1.0 {
-            redLightView.alpha = 0.3
-            yellowLightView.alpha = 1.0
-        } else if yellowLightView.alpha == 1.0 {
-            yellowLightView.alpha = 0.3
-            greenLightView.alpha = 1.0
-        } else if greenLightView.alpha == 1.0 {
-            greenLightView.alpha = 0.3
-            redLightView.alpha = 1.0
-        } else { 
-            redLightView.alpha = 1.0
+        if isFirstPress {
+            sender.setTitle("Next", for: .normal)
+            isFirstPress = false
         }
+        
+        switch currentState {
+        case .red:
+            redLightView.alpha = lightOn
+            greenLightView.alpha = lightOff
+            currentState = .yellow
+        case .yellow:
+            yellowLightView.alpha = lightOn
+            redLightView.alpha = lightOff
+            currentState = .green
+        case .green:
+            greenLightView.alpha = lightOn
+            yellowLightView.alpha = lightOff
+            currentState = .red
+        }
+        
+
     }
     
+    private func createCircleView(_ view: UIView) {
+        view.layer.cornerRadius = view.frame.size.width / 2
+    }
+    
+}
+
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
 }
